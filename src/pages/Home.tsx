f7a44827179e78ca6ac5261f2f72ca3f0283375e
MaxMagician <leon.zheng2016@gmail.com>
2026-01-16 01:15:07 -0800

    Fix build errors: Remove unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
--- src/pages/Life.tsx
+++ src/pages/Life.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const Life = () => {
